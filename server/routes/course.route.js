c259a74650e1d9dae8c8fee1fdb03f6a9bc62185
IsuruUdara2001 <isuruedirisinghe2001@gmail.com>
2025-03-28 20:23:21 +0530

    update course

--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -13,7 +13,7 @@ router.route("/:courseId").put(isAuthenticated,upload.single("courseThumbnail"),
 router.route("/:courseId").get(isAuthenticated, getCourseById);
 router.route("/:courseId/lecture").post(isAuthenticated, createLecture);
 router.route("/:courseId/lecture").get(isAuthenticated, getCourseLecture);
-router.route("/:courseId/lecture/:lectureId").post(isAuthenticated, editLecture);
+router.route("/:courseId/lecture/:lectureId").post(upload.single("videoInfo"), editLecture);
 router.route("/lecture/:lectureId").delete(isAuthenticated, removeLecture);
 
 router.route("/lecture/:lectureId").get(isAuthenticated, getLectureById);
