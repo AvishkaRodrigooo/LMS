d24b8187a6027c9115e340d72b415c351646038e
ChamikaMelan <melanwijesinghe4@gmail.com>
2025-04-30 12:40:21 +0530

    update
    update

--- client/src/pages/student/CourseDetail.jsx
+++ client/src/pages/student/CourseDetail.jsx
@@ -54,6 +54,18 @@ const CourseDetail = () => {
             <p>Last updated {course?.createdAt.split("T")[0]}</p>
           </div>
           <p>Students enrolled: {course?.enrolledStudents.length}</p>
+
+
+          <div className="button-container">
+            <button
+              className="CDPfeedbackbtn"
+              onClick={() => navigate("/feedback")}
+            >
+              See Feedbacks
+            </button>
+          </div>
+
+
         </div>
       </div>
       <div className="max-w-7xl mx-auto my-5 px-4 md:px-8 flex flex-col lg:flex-row justify-between gap-10">
